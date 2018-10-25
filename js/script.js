4d412a1f9a31fb3c36ffa8cccf2144e95d7693a1
GuilhermeBalog <guibalog@gmail.com>
2018-10-25 19:22:00 -0300

    Finalização da dinamização do sistemas e alterações de Design

--- js/script.js
+++ js/script.js
@@ -1,18 +1,45 @@
 var letra;
 var setor;
 var limite		= 75;
-var letras		= ['b','i','n','g','o'];
+var palavra 	= "bingo";
+var letras		= palavra.split('');
 var qtLetra		= letras.length;
 var perLetra	= limite/qtLetra ;
 var nums 		= [];
 var finalizado	= false;
+
 //elementos
 var title		= document.getElementById('title');
 var botao 		= document.getElementById('caller');
 var botaoReset 	= document.getElementById('rstbtn');
 var lastNumP 	= document.getElementById('lastNum');
 var numsP 		= document.getElementById('nums');
+var setores		= document.getElementById('setores');
 
+//função 
+function createCard(title, width){
+	var card = document.createElement('div');
+	card.classList.add('card');
+	card.style.width = width+"%";
+
+	var h1 = document.createElement('h1');
+	h1.innerHTML = title.toUpperCase();
+	card.append(h1);
+
+	var p = document.createElement('p');
+	p.id = title;
+	card.append(p);
+
+	return card;
+}
+
+for(var e = 0; e < letras.length; e++) {
+	var section = createCard(letras[e],(100/letras.length));
+	setores.append(section);
+}
+
+
+//função para gerar numeros inteiros aleatórios
 function randomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
@@ -25,21 +52,11 @@ function newNum(){
 
 		if(!nums.includes(newNum)){
 
-			//Verifica a letra a que o número pertence 
-			if(newNum >= (0*perLetra + 1) && newNum <= (1*perLetra) ){
-				letra = "b";
-			}
-			else if(newNum >= (1*perLetra + 1) && newNum <= (2*perLetra)){
-				letra = "i";
-			}
-			else if(newNum >= (2*perLetra + 1) && newNum <= (3*perLetra)){
-				letra = "n";
-			}
-			else if(newNum >= (3*perLetra + 1) && newNum <= (4*perLetra)){
-				letra = "g";
-			}
-			else if(newNum >= (4*perLetra + 1) && newNum <= (5*perLetra)){
-				letra = "o";
+			//verifica a letra a que o número pertence
+			for(var i = 0; i < letras.length; i++){
+				if(newNum >= ((i * perLetra) + 1) && newNum <= ((i+1)*perLetra)){
+					letra = letras[i];
+				}
 			}
 
 			setor = document.getElementById(letra);
@@ -51,7 +68,7 @@ function newNum(){
 			}
 
 			//Escreve o último número na div correspondente
-			lastNumP.innerHTML = "<h1>"+letra.toUpperCase()+"</h1><h3>"+newNum+"</h3>";
+			lastNumP.innerHTML = "<h1>"+letra.toUpperCase()+"</h1><p>"+newNum+"</p>";
 
 			//Adiciona o novo número no array dos já foram
 			nums.push(newNum);
