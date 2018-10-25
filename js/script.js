10c4fc0ed53957e1f5af7e94931d7c486199aded
GuilhermeBalog <guibalog@gmail.com>
2018-10-25 17:48:37 -0300

    Mudanças de Layout, adição de comentários e inicio da dinamização do script

--- js/script.js
+++ js/script.js
@@ -1,49 +1,79 @@
-function randomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
 var letra;
 var setor;
+var limite		= 75;
+var letras		= ['b','i','n','g','o'];
+var qtLetra		= letras.length;
+var perLetra	= limite/qtLetra ;
 var nums 		= [];
-var acabado		= false;
+var finalizado	= false;
+//elementos
 var title		= document.getElementById('title');
 var botao 		= document.getElementById('caller');
 var botaoReset 	= document.getElementById('rstbtn');
 var lastNumP 	= document.getElementById('lastNum');
 var numsP 		= document.getElementById('nums');
 
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
 function newNum(){
-	if(!acabado){
-		var newNum = randomInt(1,75);
+	if(!finalizado){
+
+		var newNum = randomInt(1,limite);
+
 		if(!nums.includes(newNum)){
-			if(newNum >= 1 && newNum <= 15){
+
+			//Verifica a letra a que o número pertence 
+			if(newNum >= (0*perLetra + 1) && newNum <= (1*perLetra) ){
 				letra = "b";
 			}
-			else if(newNum >= 16 && newNum <= 30){
+			else if(newNum >= (1*perLetra + 1) && newNum <= (2*perLetra)){
 				letra = "i";
 			}
-			else if(newNum >= 31 && newNum <= 45){
+			else if(newNum >= (2*perLetra + 1) && newNum <= (3*perLetra)){
 				letra = "n";
 			}
-			else if(newNum >= 46 && newNum <= 60){
+			else if(newNum >= (3*perLetra + 1) && newNum <= (4*perLetra)){
 				letra = "g";
 			}
-			else if(newNum >= 61 && newNum <= 75){
+			else if(newNum >= (4*perLetra + 1) && newNum <= (5*perLetra)){
 				letra = "o";
 			}
-			nums.push(newNum);
+
 			setor = document.getElementById(letra);
-			setor.innerHTML += newNum+', ';
+			//Adiciona o novo número na letra correspondente
+			if(setor.innerHTML == ''){
+				setor.innerHTML += newNum;
+			}else{
+				setor.innerHTML += ', '+newNum;
+			}
+
+			//Escreve o último número na div correspondente
 			lastNumP.innerHTML = "<h1>"+letra.toUpperCase()+"</h1><h3>"+newNum+"</h3>";
-			if(nums.length == 75){
+
+			//Adiciona o novo número no array dos já foram
+			nums.push(newNum);
+
+			//confere se o jogo foi finalizado
+			if(nums.length == limite){
 				title.innerHTML += " - Finalizado"
 				caller.disabled = 'disabled';
-				acabado = true;
+				finalizado = true;
+			}
+
+			// Escreve os números que já foram em ordem decrescente
+			for(var c = nums.length; c > 0; c--){
+				if(c == nums.length){
+					numsP.innerHTML = nums[c-1];
+				}else{
+					numsP.innerHTML += ", "+nums[c-1];
+				}
 			}
 		}else{
 			this.newNum();
 		}
-		numsP.innerHTML = "<h2>Já foram:</h2>"+nums.join(', ');
 	}
 }
 
